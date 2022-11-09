94534119c0afef229e299bb050fcddd2ac248f03
Michael <45869980+mdouros@users.noreply.github.com>
2022-11-09 00:08:45 -0600

    Add information on each strat tag

--- drg-strat-roulette/src/app/models/strat.interface.ts
+++ drg-strat-roulette/src/app/models/strat.interface.ts
@@ -17,14 +17,63 @@ export interface Strategy {
 }
 
 export enum StratTag {
-	settings = 'This strategy requires changing in-game settings.',
-	loadout = 'This strategy requires bringing a particular loadout. You will need to modify a loadout slot to complete it.',
-	time = 'This strategy may significantly increase the time taken to complete a mission.',
-	communication = 'This strategy requires a high level of communication. Voice-chat is strongly recommended.',
-	queue = 'This strategy relies on RNG to be attempted. As such, they will be queued up and attempted when the opportunity presents itself.',
-	nausea = 'This strategy has the potential to cause nausea for some players.',
-	class = 'This strategy requires multiple dwarves to play as the same class.',
-	advancedSettings = 'This strategy requires advanced changes to settings. It will require editing settings files in a text editor and/or restarting your game.',
+	settings = 'Settings',
+	loadout = 'Loadout',
+	time = 'Time',
+	communication = 'Communication',
+	queue = 'Queue',
+	nausea = 'Nausea',
+	class = 'Class',
+}
+
+export const stratTagInfo: StratTagInfo[] = [
+	{
+		type: StratTag.settings,
+		description: 'This strategy requires changing in-game settings.',
+		tooltipDetails: 'require changing in-game settings.',
+	},
+	{
+		type: StratTag.loadout,
+		description:
+			'This strategy requires bringing a particular loadout. You will need to modify a loadout slot to complete it.',
+		tooltipDetails: 'will require me to change my equipment loadout.',
+	},
+	{
+		type: StratTag.time,
+		description: 'This strategy may significantly increase the time taken to complete a mission.',
+		tooltipDetails: 'may significantly increase the mission time.',
+	},
+	{
+		type: StratTag.communication,
+		description: 'This strategy requires a high level of communication. Voice-chat is strongly recommended.',
+		tooltipDetails: 'necessitate voice communications.',
+	},
+	{
+		type: StratTag.queue,
+		description:
+			'This strategy relies on RNG to be attempted. As such, they should be queued up and attempted when the opportunity presents itself.',
+		tooltipDetails: 'must be queued.',
+	},
+	{
+		type: StratTag.nausea,
+		description: 'This strategy has the potential to cause nausea for some players.',
+		tooltipDetails: 'may cause nausea.',
+	},
+	{
+		type: StratTag.class,
+		description: 'This strategy requires multiple dwarves to play as the same class.',
+		tooltipDetails: 'require multiple dwarves to play as the same class.',
+	},
+];
+
+export interface StratTagInfo {
+	type: StratTag;
+	description: string;
+	tooltipDetails: string;
+}
+
+export interface StratTagObject extends StratTagInfo {
+	checked: boolean;
 }
 
 export enum ExclusionCategory {
