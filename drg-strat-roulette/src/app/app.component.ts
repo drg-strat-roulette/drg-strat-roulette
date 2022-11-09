2d7de681c8fdcc2afb1480f5a837433fe6801d64
Michael <45869980+mdouros@users.noreply.github.com>
2022-11-09 00:10:54 -0600

    - Allow for strategies to be rolled - Allow for tags to be excluded

--- drg-strat-roulette/src/app/app.component.ts
+++ drg-strat-roulette/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { strategies } from './data/strats.const';
+import * as lodash from 'lodash';
+import { Strategy, stratTagInfo, StratTagObject } from './models/strat.interface';
 
 @Component({
 	selector: 'app-root',
@@ -7,4 +10,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title = 'drg-strat-roulette';
+	strat: Strategy | undefined;
+	tags: StratTagObject[] = stratTagInfo.map((tagInfo) => ({
+		...tagInfo,
+		checked: true,
+	}));
+
+	rollStrat() {
+		const excludedTags = this.tags.filter((tag) => !tag.checked).map((tag) => tag.type);
+		const candidateStrats = strategies.filter((strat) => !strat.tags?.some((tag) => excludedTags.includes(tag)));
+		this.strat = lodash.sample(candidateStrats);
+	}
 }
