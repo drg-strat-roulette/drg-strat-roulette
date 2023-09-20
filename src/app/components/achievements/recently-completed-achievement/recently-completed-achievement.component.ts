128f4bc0a7f764dadb5952054dd65700418f394b
Michael <mddouros98@gmail.com>
2023-09-20 01:46:20 -0500

    Improve time accuracy of recent achievement alerts and change undo time to 7 seconds

--- src/app/components/achievements/recently-completed-achievement/recently-completed-achievement.component.ts
+++ src/app/components/achievements/recently-completed-achievement/recently-completed-achievement.component.ts
@@ -2,7 +2,8 @@ import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angu
 import { Subject, interval, takeUntil } from 'rxjs';
 import { DisplayedAchievement } from 'src/app/models/achievement.model';
 
-const MAX_LIFETIME = 10_000;
+// Time before this alert auto-disappears
+const ALERT_LIFETIME_MS = 7_000;
 
 @Component({
 	selector: 'app-recently-completed-achievement',
@@ -22,16 +23,19 @@ export class RecentlyCompletedAchievementComponent implements OnInit, OnDestroy 
 	/** Counts 0-100 throughout the defined lifetime */
 	timeAlive = 0;
 
+	private start = Date.now();
 	private destroy: Subject<void> = new Subject();
+
 	constructor() {}
 
 	ngOnInit(): void {
-		// Tick timeAlive every 1/100th of MAX_LIFETIME
-		interval(MAX_LIFETIME / 100)
+		// Update progress every ~50ms (Less often due to processing delays)
+		interval(50)
 			.pipe(takeUntil(this.destroy))
 			.subscribe(() => {
-				this.timeAlive += 1;
-				// Kill component once MAX_LIFETIME is reached
+				// Recompute percentage of way to ALERT_LIFETIME_MS
+				this.timeAlive = ((Date.now() - this.start) * 100) / ALERT_LIFETIME_MS;
+				// Kill component once ALERT_LIFETIME_MS is reached
 				if (this.timeAlive >= 100) {
 					this.kill.emit();
 				}
